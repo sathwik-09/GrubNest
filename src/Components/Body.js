56599cc5083d47e350461b7eb60c08a1652a39fb
Sathwik <enapothulasathwik09@gmail.com>
2025-01-30 12:01:41 +0530

    used state variables

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
+
 const Body = () => {
+  const [RestaurantList, setRestaurantList] = useState(resList);
+  
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filteredRes = RestaurantList.filter((res)=>res.info.avgRating>4);
+            setRestaurantList(filteredRes);
+          }}
+        >
+          Search Top Rated Restaurants
+        </button>
+      </div>
       <div className="restro-container">
-        {resList.map((restaurant) => (
+        {RestaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
