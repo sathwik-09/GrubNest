9731e661ba623c7275922722f3a266a95e36d6e1
Sathwik <enapothulasathwik09@gmail.com>
2025-02-01 21:27:51 +0530

    added extra feature

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +19,9 @@ const Header = () => {
           <li>About us</li>
           <li>Contact us</li>
           <li>Cart</li>
+          <button className="login" onClick={()=>{
+            btnName==="Login" ? setBtnName("Logout") : setBtnName("Login");
+          }}>{btnName}</button>
         </ul>
       </div>
     </div>
