4732750a27a82661685d5bd9a734c49fbaedc1fe
Sathwik <enapothulasathwik09@gmail.com>
2025-02-02 22:59:34 +0530

    about page

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return(
+    <div>
+      <h1>Welcome to GrubNest</h1>
+      <h2>Order anything without minimum order and delivery fee</h2>
+      <h2>Enjoy your meal</h2>
+    </div>
+  )
+}
+export default About;
