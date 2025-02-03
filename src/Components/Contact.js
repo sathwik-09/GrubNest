1a9bc5cbbc2e5c4ae1447885de532dc93224b56f
Sathwik <enapothulasathwik09@gmail.com>
2025-02-03 22:52:39 +0530

    added routes

---
+++ src/Components/Contact.js
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+  return(
+    <div>
+      <h1>Cotact us</h1>
+      <h2>+919999999999</h2>
+      <h2>namastefood@outlook.com</h2>
+    </div>
+  )
+}
+
+export default Contact;
