3e56103e58a18329b74ae01a403cf16071a7f9d2
Sathwik <enapothulasathwik09@gmail.com>
2025-02-04 23:22:05 +0530

    Chunking

---
+++ src/Components/Grocery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Grocery = () => {
+  return (
+    <div>
+    <h1>Welcome to the grocery store</h1>
+    <h2>Get Rs 100 on your first order</h2>
+    </div>
+  )
+}
+
+export default Grocery
